16e1e1e81089eafb15a734170ea43d3d523ced34
Aaron Chavez <achavez@corporaciondpt.com>
2018-08-22 14:43:51 -0600

    finished class 7

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';(test)
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // countries: any[] = []; test
+
+  constructor(  ) { // private http: HttpClient (insde the constructor like injection this is a test)
+
+   /* this.http.get('https://restcountries.eu/rest/v2/lang/es').subscribe( (resp:any) => { //here you put the variable as any to match the global variable (test)
+      console.log(resp); // you subscribe to the get methos and the things you get would be safe on the resp variable (test)
+      this.countries = resp;
+    });*/
+
+  }
 
   ngOnInit() {
   }
